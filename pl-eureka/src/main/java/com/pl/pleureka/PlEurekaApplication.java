9e2c6feae7e130240f5b248b643e034cbc2a0beb
LAPTOP-9CFRV1LA\liupan <pl13203802214@163.com>
2020-10-09 20:02:47 +0800

    first commit

---
+++ pl-eureka/src/main/java/com/pl/pleureka/PlEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.pl.pleureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlEurekaApplication.class, args);
+    }
+
+}
---
+++ pl-eureka/src/test/java/com/pl/pleureka/PlEurekaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.pl.pleureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("test");
+    }
+
+}
