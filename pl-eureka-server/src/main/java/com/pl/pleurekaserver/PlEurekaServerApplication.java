43320e22fb4e977dd02228f9ee986f37cbf11986
liupanone <pl13203802214@163.com>
2020-10-10 11:29:42 +0800

    add pl-eureka-server

---
+++ pl-eureka-server/src/main/java/com/pl/pleurekaserver/PlEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.pl.pleurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlEurekaServerApplication.class, args);
+    }
+
+}
---
+++ pl-eureka-server/src/test/java/com/pl/pleurekaserver/PlEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.pleurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
